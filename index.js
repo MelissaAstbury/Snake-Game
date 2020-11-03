6de707a7a4e10d4cf2d43f6218a550bc5622551b
Melissa Astbury <melissaastbury@hotmail.com>
2020-11-03 16:36:29 +0000

    css styking added and also new folder created to hold to snake component seperatly

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 const canvas = document.getElementById('canvas');
-// const ctx = canvas.getContext('2d');
+const ctx = canvas.getContext('2d');
+const scale = 10;
+const rows = canvas.height / scale;
+const columns = canvas.width / scale;
