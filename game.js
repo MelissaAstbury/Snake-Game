0f8986720f372cb6d582cde3cd3d60453ad0991f
Melissa Astbury <melissaastbury@hotmail.com>
2020-11-04 14:34:32 +0000

    the segments of the snake are now rendered on the canvas moving alond the x axis. Functionality to 'forget' the previous segement have also been put in so the snake moves along without getting bigger in length

--- game.js
+++ game.js
@@ -1,19 +1,25 @@
+const canvas = document.getElementById('canvas');
+
 import {
   update as updateSnake,
   draw as drawSnake,
   SNAKE_SPEED,
 } from './snake.js';
 
-//create a continous loop - this is needed as this will control the location of the snake on the page - the more times it is rendered the more smooth the transaction will be
+/* 
+Create a continous loop - this is needed as this will control the location of the snake
+ on the page - the more times it is rendered the more smooth the transaction will be
+ */
+
 let lastRenderTime = 0;
 
 function main(currentTime) {
   window.requestAnimationFrame(main);
-  // Get the result of how fast the page rendered in seconds instead of milliseconds
+  /* Get the result of how fast the page rendered in seconds instead of milliseconds */
   const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
 
   if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
-  // 1 / 2 = 0.5
+  /* 1 / 2 = 0.5 */
   lastRenderTime = currentTime;
 
   update();
@@ -27,5 +33,10 @@ function update() {
 }
 
 function draw() {
-  drawSnake();
+  /* 
+  For the game to forget the previous position once all segments are passed 
+  we need to set the HTML to nothing
+  */
+  canvas.innerHTML = '';
+  drawSnake(canvas);
 }
--- snake.js
+++ snake.js
@@ -1,9 +1,38 @@
-export const SNAKE_SPEED = 2;
-
+export const SNAKE_SPEED = 1;
+/* 
+Now declare where the snake will start 
+*/
+const snakeBody = [
+  { x: 10, y: 11 },
+  { x: 11, y: 11 },
+  { x: 12, y: 11 },
+];
+/*  
+This function needs to move the snake around. However, we need to make sure that when 
+the snake move to the left or right, that only the first one is moved and the rest 
+follow the pattern
+*/
 export function update() {
-  console.log('update snake');
+  for (let i = snakeBody.length - 2; i >= 0; i--) {
+    snakeBody[i + 1] = { ...snakeBody[i] };
+  }
+  snakeBody[0].x += 1;
+  snakeBody[0].y += 0;
 }
 
-export function draw() {
-  console.log('draw snake');
+/* 
+This function will draw the snake out on the 'segment' of the grid - to do this 
+the div of the 'canvas' must be passed 
+*/
+export function draw(canvas) {
+  snakeBody.forEach((segment) => {
+    /* Create the snake itself */
+    const snakeElement = document.createElement('div');
+    /* The segment is each piece of the snake */
+    snakeElement.style.gridRowStart = segment.y;
+    snakeElement.style.gridColumnStart = segment.x;
+    snakeElement.classList.add('snake');
+    /* Now place the snake on the 'canvas' */
+    canvas.appendChild(snakeElement);
+  });
 }
