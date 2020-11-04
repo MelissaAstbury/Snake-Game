3914bbd7d24e618d657584463d1871afe9a07282
Melissa Astbury <melissaastbury@hotmail.com>
2020-11-04 14:49:48 +0000

    the snake can no longer go back on itself. For example, if the snake it travelling right we can not go left

--- snake.js
+++ snake.js
@@ -1,23 +1,23 @@
-export const SNAKE_SPEED = 1;
+import { getInputDirection } from './input.js';
+
+export const SNAKE_SPEED = 3;
 /* 
 Now declare where the snake will start 
 */
-const snakeBody = [
-  { x: 10, y: 11 },
-  { x: 11, y: 11 },
-  { x: 12, y: 11 },
-];
+const snakeBody = [{ x: 11, y: 11 }];
 /*  
 This function needs to move the snake around. However, we need to make sure that when 
 the snake move to the left or right, that only the first one is moved and the rest 
 follow the pattern
 */
 export function update() {
+  const inputDirection = getInputDirection();
+
   for (let i = snakeBody.length - 2; i >= 0; i--) {
     snakeBody[i + 1] = { ...snakeBody[i] };
   }
-  snakeBody[0].x += 1;
-  snakeBody[0].y += 0;
+  snakeBody[0].x += inputDirection.x;
+  snakeBody[0].y += inputDirection.y;
 }
 
 /* 
